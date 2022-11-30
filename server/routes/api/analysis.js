47a5396a4bc568e662cd834e092d38c274dc70b4
MiscellaneousStuff <joe.makepeace01@gmail.com>
2022-11-30 17:35:35 +0000

    Added graceful 400 error for missing analytics file on download

--- server/routes/api/analysis.js
+++ server/routes/api/analysis.js
@@ -1,6 +1,7 @@
 // Express
 const express = require("express");
 const router = express.Router();
+const fs = require("fs");
 
 /**
  * @route GET api/analysis/download
@@ -12,7 +13,11 @@ const router = express.Router();
     try {
         let stream = req.query.stream;
         const downloadPath = `./server/analysis/${stream}.csv`;
-        res.download(downloadPath);
+        if (fs.existsSync(downloadPath)) {
+            return res.download(downloadPath);
+        } else {
+            return res.status(400).send(`Error: Analytics file for ${stream} doesn't exist.`)
+        }
     } catch (err) {
         console.error(err);
         return res.status(424).send("Error downloading video stream analysis file");
