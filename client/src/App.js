8027288ac1808d7ef6956bdbc7bc8fe25a699d7e
MiscellaneousStuff <joe.makepeace01@gmail.com>
2022-12-09 15:05:44 +0000

    Added redux explanation to App.js in client

--- client/src/App.js
+++ client/src/App.js
@@ -1,3 +1,13 @@
+/*
+Wraps all of the routes (i.e. the client URL routes along with the
+correct component) in a Redux provider. Redux is a react library
+which allows you to globally manage the state of your application
+while avoiding the hell of continuously providing props to the children
+of a component recursively. Instead, you just wrap you function around
+redux and you are given one prop which allows you to execute functions
+or dispatch actions which affect the global state of the application.
+*/
+
 import './App.css';
 
 import { Provider } from "react-redux";
