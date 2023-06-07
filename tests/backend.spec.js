1c81741b123aa4efef86a9d76f51b5e0dc209ce7
Alvie <contact@alviemahmud.com>
2023-06-07 06:08:30 +0100

    #44 Backend Unit Testing: - installed jest + supertest - removed / in proxy - created /tests/backend.spec.js file - added video to /tests/resources/videos

---
+++ tests/backend.spec.js
@@ -0,0 +1,25 @@
+const request = require('supertest');
+
+describe('Backend test suite', () => {
+    it('tests /api/init endpoint', async() => {
+
+        // arrange
+        // add video file to be analysed
+        // Already added in /tests/resources/test_video_1.mp4
+
+        // act
+        // send request to /api/init
+        const response = await request('http://localhost:6000')
+            .post("/api/init")
+            .send({
+                "stream": __dirname + "/resources/test_video_1.mp4"
+            });
+
+        // assert
+        // expect response to be 200
+        // expect text to be "Video stream analysis successfully started"\n
+        expect(response.statusCode).toBe(200);
+        expect(response.text).toBe('"Video stream analysis successfully started"\n');
+    });
+
+});
