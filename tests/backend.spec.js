50320ea24bb206009928c78326e94f36fb2ff617
Alvie <contact@alviemahmud.com>
2023-06-13 14:56:24 +0100

    #44 Backend tests:  - added /api/streams/deleteVideo test  - added /api/analysis test

--- tests/backend.spec.js
+++ tests/backend.spec.js
@@ -1,8 +1,84 @@
 const request = require('supertest');
 const fs = require('fs');
 const path = require('path');
+const { response } = require('express');
 
 describe('Backend test suite', () => {
+    // NEED TO ADD TESTS FOR /api/streams/add
+    // NEED TO ADD TESTS FOR /api/streams/edit
+
+
+    it('tests posting to /api/analysis endpoint', async () => {
+        // arrange
+        // upload video to be analyzed
+        const potentialExistingFilePath = path.join(__dirname, '..', 'server', 'streams', 'test_video_1.mp4');
+        if (fs.existsSync(potentialExistingFilePath)) {
+            fs.unlinkSync(potentialExistingFilePath);
+        }
+
+        const filePath = path.join(__dirname, 'resources', 'videos', 'test_video_1.mp4');
+        const uploadVideoResponse = await request('http://localhost:3000')
+            .put('/api/streams/upload')
+            .attach('stream', filePath);
+
+        const initResponse = await request('http://localhost:3000')
+            .post("/api/init")
+            .send({
+                "stream": filePath
+            });
+
+        // act
+        // send request to /api/analysis
+        const response = await request('http://localhost:3000')
+            .post("/api/analysis")
+            .send({
+                "stream": "test_video_1.mp4",
+                "start": 1,
+                "end": 10,
+            });
+
+        // assert
+        // expect response to be 200
+        try {
+            expect(response.statusCode).toBe(200);
+        } catch (err) {
+            console.error(response.body);
+        }
+        expect(response.body).toHaveProperty("metadata");
+        expect(response.body).toHaveProperty("tracking_format");
+        expect(response.body).toHaveProperty("counts");
+        expect(response.body).toHaveProperty("routes");
+    }, 30000);
+
+    it('tests deleting from /api/streams/deleteVideo endpoint', async () => {
+        // arrange
+        // upload video to be deleted
+        const potentialExistingFilePath = path.join(__dirname, '..', 'server', 'streams', 'test_video_1.mp4');
+        if (fs.existsSync(potentialExistingFilePath)) {
+            fs.unlinkSync(potentialExistingFilePath);
+        }
+        
+        const filePath = path.join(__dirname, 'resources', 'videos', 'test_video_1.mp4');
+        const uploadVideoResponse = await request('http://localhost:3000')
+        .put('/api/streams/upload')
+        .attach('stream', filePath);
+
+        // act
+        // send request to /api/streams/deleteVideo
+        const response = await request('http://localhost:3000')
+            .post("/api/streams/deleteVideo")
+            .send({
+                "source": "test_video_1.mp4"
+            });
+
+        // assert
+        // expect response to be 200
+        // expect response.text to be "video deleted from directory"
+        expect(response.statusCode).toBe(200);
+        expect(response.text).toBe('video deleted from directory');
+    }, 10000);
+
+
     it('tests putting to /api/streams/upload endpoint', async () => {
         // arrange
         // Remove if already exists so test is accurate
